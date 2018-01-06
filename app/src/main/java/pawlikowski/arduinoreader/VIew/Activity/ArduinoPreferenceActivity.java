7d9a4c0bda00406622c96be9cb6e1d5cd15de00d
SPToss <sebastian.lukasz.pawlikowski@gmail.com>
2018-01-06 17:22:12 +0100

    Commit

---
+++ app/src/main/java/pawlikowski/arduinoreader/VIew/Activity/ArduinoPreferenceActivity.java
@@ -0,0 +1,32 @@
+package pawlikowski.arduinoreader.VIew.Activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import pawlikowski.arduinoreader.R;
+
+/**
+ * Created by Sebas on 06.01.2018.
+ */
+
+public class ArduinoPreferenceActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preference);
+
+        }
+    }
+
+}
